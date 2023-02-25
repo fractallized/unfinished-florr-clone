2cc35679d5463d31a2c475f43e36379e1a70bb37
Test-Helper-js <126033859+Test-Helper-js@users.noreply.github.com>
2023-02-25 23:36:48 +0000

    removed express dependency

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,53 @@
-import * as HTTP from "http";
+import * as http from "http";
 import * as WebSocket from "ws";
 import * as fs from "fs";
-import express = require("express");
 
 import GameServer from "./game/Server";
 const PORT = 1025;
 
-class Server {
-  app = express();
-  constructor() {}
-  addGetReq(path: string, file: string, type: string) {
-    this.app.get(path, (_, res) => {
-      res.setHeader("Content-Type", `${type}; charset=utf-8`);
+const server = http.createServer((req, res) => {
+      let file: string | null = null;
+      let contentType = "text/html"
+      switch (req.url) {
+          case "/":
+              file = "./client/index.html";
+              contentType = "text/html";
+              break;
+          case "/index.js":
+              file = "./client/index.js";
+              contentType = "application/javascript";
+              break;
+          case "/input.js":
+              file = "./client/input.js";
+              contentType = "application/javascript";
+              break;
+          case "/client-render.js":
+              file = "./client/client-render.js";
+              contentType = "application/javascript";
+              break;
+          case "/canvas-helpers.js":
+              file = "./client/canvas-helpers.js";
+              contentType = "application/javascript";
+              break;
+          case "/websocket.js":
+            file = "./client/websocket.js";
+            contentType = "application/javascript";
+            break;
+          case "/fonts.css":
+            file = "./client/fonts.css";
+            contentType = "text/css";
+            break;
+      }
+
+      res.setHeader("Content-Type", contentType + "; charset=utf-8");
+
       if (file && fs.existsSync(file)) {
-        res.writeHead(200);
-        return res.end(fs.readFileSync(file));
+          res.writeHead(200);
+          return res.end(fs.readFileSync(file));
       }
       res.writeHead(404);
-    });
-    return this;
-  }
-}
+});
+/*
 const _server = new Server()
 .addGetReq("/","client/index.html","text/html")
 .addGetReq("/fonts.css","client/fonts.css","text/css")
@@ -31,7 +58,8 @@ const _server = new Server()
 .addGetReq("/websocket.js","client/websocket.js","application/javascript");
 const HTTPServer = HTTP.createServer(_server.app);
 HTTPServer.listen(PORT);
-
-const Socket = new WebSocket.Server({ server: HTTPServer });
+*/
+server.listen(PORT);
+const Socket = new WebSocket.Server({ server });
 new GameServer(Socket);
 console.log("[Server is running]");
